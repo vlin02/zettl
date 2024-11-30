a442ed3bb5ccc729ed70ed06af979a1d7279699a
Victor Lin <vvictor.llin@gmail.com>
2024-11-30 07:54:21 -0800

    rev

---
+++ frontend/vite.config.ts
@@ -0,0 +1,19 @@
+import { resolve } from "path"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+
+export default defineConfig({
+  plugins: [react()],
+  clearScreen: false,
+  server: {
+    strictPort: true
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        popover: resolve(__dirname, "popover.html"),
+        settings: resolve(__dirname, "settings.html")
+      }
+    }
+  }
+})
